a1645e26681bdcec708d4c9a06c24320095a0493
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-30 15:29:18 +0600

    Find even and odd number

---
+++ OddEven.js
@@ -0,0 +1,20 @@
+//Even Number
+
+let num = 61;
+console.log("***** Even numbers from 61 to 100. *****")
+while (num <= 100) {
+    if (num % 2 === 0) {
+        console.log(num)
+
+    } num++;
+}
+
+//Odd number
+console.log("***** Odd numbers from 78 to 98. *****")
+let n=78;
+while (n <=98) {
+    if (n % 2 !=0) {
+        console.log(n)
+    }
+    n++;
+}
