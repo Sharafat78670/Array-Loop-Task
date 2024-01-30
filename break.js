4413882c9207f7998bbc14c2671c91531f38d4c8
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-30 22:44:19 +0600

    Add break and continue js file

---
+++ break.js
@@ -0,0 +1,20 @@
+//number break
+
+/*for(let n=1;n<=200;n++){
+    if (n==100) {
+        break;
+    }
+    console.log(n);
+}*/
+
+//sum break
+let sum=0;
+for(let n=1;n<=100;n++){
+    sum+=n;
+    if (sum >= 100) {
+        break;          
+    }
+   console.log(n);
+  
+} 
+ 
---
+++ continue.js
@@ -0,0 +1,16 @@
+
+// for (let num = 1; num <= 40; num++) {
+//     if (num%2!=0) {
+//        continue;
+//     }
+//    console.log(num);        
+// }
+
+for (let num = 55; num <=85; num++) {
+     
+    if (num%5!=0) {
+       continue
+    }
+           console.log(num);
+
+}
