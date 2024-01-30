5f862c902cf48efe3d393bcb39ff6e3a4bccf8be
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-30 15:50:44 +0600

    Multiplication

---
+++ multiplication.js
@@ -0,0 +1,9 @@
+
+let number=1;
+for(let m=1;m<=10;m++){
+    console.log("Multiplication Table : Number",m)
+    for(let n=1;n<=10;n++){
+        number=m*n;
+        console.log(m,"*",n,"=",number)
+    }
+}
